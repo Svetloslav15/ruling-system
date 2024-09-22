5a707866bab0127a7fb8cc66d4a0ddee9e42e7f4
Svetloslav15 <svetli.novoselski@gmail.com>
2024-09-22 09:47:23 +0300

    setup logger

---
+++ src/utils/logger.js
@@ -0,0 +1,19 @@
+import winston from 'winston';
+import dotenv from 'dotenv';
+
+const FILE_NAME = 'app.log';
+dotenv.config();
+
+export const createLogger = () => {
+  return winston.createLogger({
+    level: process.env.LOG_LEVEL,
+    format: winston.format.combine(
+      winston.format.timestamp(),
+      winston.format.json()
+    ),
+    transports: [
+      new winston.transports.Console(),
+      new winston.transports.File({ filename: FILE_NAME }),
+    ],
+  });
+};
